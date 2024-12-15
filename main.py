c6aa4c0e6f12e235c837db14c0307ed7a4749d75
sunkam836 <sunkam836@gmail.com>
2024-12-15 22:39:47 +1200

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 def get_multiplied_digits(number):
     str_number = str(number)
+    first = int(str_number[0])
+
     if len(str_number) > 1:
-        first = int(str_number[0])
         return first*get_multiplied_digits(int(str_number[1:]))
     else:
-        return int(str_number)
+        return first if first != 0 else 1
+
 
 result = get_multiplied_digits(40203)
 print(result)
 
 result2 = get_multiplied_digits(402030)
 print(result2)
-
-result3 = get_multiplied_digits(4020302)
-print(result3)
